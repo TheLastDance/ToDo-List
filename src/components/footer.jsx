db0e5b23333b77d035f470e01f932bb842f2cbfc
TheLastDance <89349907+TheLastDance@users.noreply.github.com>
2022-09-20 23:21:23 +0400

    Delete src directory

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-
-
-    return (
-        <footer><div><p>Coded by</p><a target='_blank' rel="noopener noreferrer" href='https://github.com/TheLastDance'>TheLastDance</a></div></footer>
-    )
-}
-
-export default Footer;
